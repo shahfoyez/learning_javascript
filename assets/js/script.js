8028396be4ceefabb1d226530f73a02e6eeae52c
shahfoyez <shahfayez@staffasia.org>
2022-11-28 12:41:23 +0600

    28/11/c2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
 var a = document.getElementById("demo").innerHTML = "Hi";
-console.log(a);
+const spacing = '10px';
+const styles =
+      `padding: ${spacing}; background-color: white; color: green; font-style:
+       italic; border: 1px solid black; font-size: 2em;`;
+console.log('%c'+a, styles);
+ 
