703284ba8891c23bbfde40ce3d0ec4934a2e9baf
shahfoyez <shahfoyez7@gmail.com>
2022-11-29 17:59:01 +0600

    29/11/c7

--- assets/js/object.js
+++ assets/js/object.js
@@ -22,3 +22,13 @@ console.log(person['id']);
 const num = new Number(5);
 console.log(num);
 document.getElementById('object_text').innerHTML = "Name: " + car.name +" speed: "+car.name;
+
+// String
+let text = "We are the so-called from the north.";
+document.getElementById('str_length').innerHTML =  text.length;
+document.getElementById('str_slice').innerHTML =  text.slice(3, 15);
+document.getElementById('str_slice').innerHTML =  text.slice(5);
+document.getElementById('str_sub').innerHTML =  text.substring(7, 13);
+document.getElementById('str_replace').innerHTML =  text.replace(/NORTH/i, 'south');
+
+
--- object.php
+++ object.php
@@ -4,14 +4,30 @@
             <h1 id="demo" class="foy-text-center fow-w-300 foy-m-0-a">Object</h1>
             <h1 id="object_text" class="foy-text-center fow-w-300 foy-m-0-a"></h1>
 
-            <h1 id="demo" class="foy-text-center fow-w-300 foy-m-0-a">Event</h1>
+            <h1 id="demo" class="foy-text-center fow-w-500 foy-m-0-a  mt-80">Event</h1>
             <div class="foy-flex foy-justify-space-beetween fow-w-400 mt-10 foy-m-0-a">
                 <button class="foy-m-0-a" onclick="document.getElementById('event').innerHTML = Date()">Date?</button>
-                <button class="foy-m-0-a" onclick="this.innerHTML = Date()">Date?</button>
+                <!-- <button class="foy-m-0-a" onclick="this.innerHTML = Date()">Date?</button> -->
 
                 <button class="foy-m-0-a" onmouseover="this.innerHTML = Date()" onmouseout="this.innerHTML = 'Date?'">Date?</button>
             </div> 
             <p id="event" class="foy-text-center"></p>
+
+            <h1 id="demo" class="foy-text-center fow-w-300 foy-m-0-a mt-80">String Method</h1>
+            <div class="foy-flex foy-justify-space-beetween fow-w-400 mt-10 foy-m-0-a">
+                
+            </div>
+            <p class="foy-text-center foy-bold m-0">String Length</p>
+            <p id="str_length" class="foy-text-center m-0"></p>
+
+            <p class="foy-text-center foy-bold m-0">String Slice</p>
+            <p id="str_slice" class="foy-text-center m-0"></p>
+
+            <p class="foy-text-center foy-bold m-0">String Slice</p>
+            <p id="str_sub" class="foy-text-center m-0"></p>
+
+            <p class="foy-text-center foy-bold m-0">String Slice</p>
+            <p id="str_replace" class="foy-text-center m-0"></p>
         </div>
     </div>
     
