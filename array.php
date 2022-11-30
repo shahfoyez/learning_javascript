d432e22e5adfdfb6312f98a43ae7917c0b4fd2f7
shahfoyez <shahfoyez7@gmail.com>
2022-11-30 17:57:16 +0600

    30/11/c2

---
+++ array.php
@@ -0,0 +1,18 @@
+<?php include 'shared/header.php';?>
+    <div class="foy-main">
+        <div class="foy-container">
+            <h1 id="demo" class="foy-text-center fow-w-300 foy-m-0-a">Array</h1>
+            <h1 id="demo2" class="foy-text-center fow-w-300 foy-m-0-a mt-50">String Method</h1>
+            
+            <p class="foy-text-center foy-bold m-0">String Length</p>
+            <p id="str_length" class="foy-text-center m-0"></p>
+
+        </div>
+    </div>
+   
+    <script src="./assets/js/array.js"></script>
+
+    <script>
+        
+    </script>
+<?php include 'shared/footer.php';?>
---
+++ assets/js/array.js
@@ -0,0 +1,47 @@
+let car = {
+    name: "Toyota",
+    model: "X16",
+    owner: "foyez",
+    speed: function() {
+        return 15;
+    }
+};
+console.log(car.speed());
+
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+const cars = ['Volvo', 'Toyota', 'BMW', 'Honda'];
+cars[4] = "new car";
+console.log(Array.isArray(cars));
+ 
+ 
+
+// // String
+// let text = "We are the so-called from the north.";
+// document.getElementById('str_length').innerHTML =  text.length;
+// document.getElementById('str_slice').innerHTML =  text.slice(3, 15);
+// document.getElementById('str_slice').innerHTML =  text.slice(5);
+// document.getElementById('str_sub').innerHTML =  text.substring(7, 13);
+// document.getElementById('str_replace').innerHTML =  text.replace(/NORTH/i, 'south');
+
+// let searchString = "This an demo string to test string search.";
+// document.getElementById('indexOf').innerHTML =  searchString.indexOf('search');
+// document.getElementById('lastIndexOf').innerHTML =  searchString.lastIndexOf('demo');
+// document.getElementById('search').innerHTML =  searchString.search('This');
+// document.getElementById('match').innerHTML =  searchString.match('string');
+// document.getElementById('matchAll').innerHTML =  Array.from(searchString.matchAll('string'));
+// document.getElementById('includes').innerHTML =  searchString.includes('acn');
+// document.getElementById('startsWith').innerHTML =  searchString.startsWith('This');
+// document.getElementById('endsWith').innerHTML =  searchString.endsWith('earch.');
+
+ 
+ 
+
+
+
