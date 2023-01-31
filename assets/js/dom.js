e0d1f9b8d51826536d25b2ac7b6151584e49af00
shahfoyez <shahfoyez7@gmail.com>
2023-01-31 17:00:19 +0600

    31/1/c4

---
+++ assets/js/dom.js
@@ -0,0 +1,7 @@
+document.getElementById('id1').innerText = "Okay";
+const p =  document.getElementById('id1');
+p.innerHTML = "Hello";
+console.log(document.getElementById('id1'));
+console.log(document.URL);
+
+
