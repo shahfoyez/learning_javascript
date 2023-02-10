6a95db1d222f8c04d3ea1f992525553a18dce943
shahfoyez <shahfoyez7@gmail.com>
2023-02-10 17:01:57 +0600

    10/2/c2

---
+++ bom.php
@@ -0,0 +1,15 @@
+<?php include 'shared/header.php';?>
+    <div class="foy-main"  >
+        <div class="foy-container">
+            <h1 id="demo" class="foy-text-center fow-w-300 foy-m-0-a mb-10">BOM</h1>
+            <div class="foy-text-center mb-10 foy-parent" id="foy-parent">
+                
+            </div> 
+        </div>
+    </div>
+    <script>
+      window.resizeTo(10, 200);
+      console.log(window.open());
+    </script>
+<?php include 'shared/footer.php';?>
+
